274f3e57b3e93bb4054cc16eeb6e2fb4faf74814
fjourdren <flavien.jourdren@gmail.com>
2020-11-25 09:35:13 +0100

    Add language selection for translation feature

--- app/src/main/java/fr/enssat/babelblock/jourdren_duchene/TranslatorActivity.kt
+++ app/src/main/java/fr/enssat/babelblock/jourdren_duchene/TranslatorActivity.kt
@@ -1,12 +1,15 @@
-package fr.enssat.babelblock.jourdren_duchene
+/*package fr.enssat.babelblock.jourdren_duchene
 
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import com.google.mlkit.nl.translate.TranslateLanguage
 import fr.enssat.babelblock.jourdren_duchene.services.translation.TranslatorService
 import kotlinx.android.synthetic.main.activity_translator.*
-import java.util.*
+
 
 // inherit BaseActivity to manage menuInflater
-class TranslatorActivity : BaseActivity() {
+class TranslatorActivity: BaseActivity(), AdapterView.OnItemSelectedListener {
 
     lateinit var translator: TranslatorService
 
@@ -16,8 +19,115 @@ class TranslatorActivity : BaseActivity() {
         // set activity's layout
         setContentView(R.layout.activity_translator)
 
+        // spinner
+        val to_language_spinner = findViewById<Spinner>(R.id.to_language_spinner)
+
+
+
+        val adapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        to_language_spinner.adapter = adapter
+
+
+        /*var languages = TranslateLanguage.getAllLanguages().toTypedArray()
+        languages = languages.map { TranslateLanguage.fromLanguageTag(it) }.toTypedArray()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, languages)
+        spinner.adapter = adapter*/
+
+
         // create translation service
-        this.translator = TranslatorService(this, Locale.FRENCH, Locale.ENGLISH, {
+        /*translate_button.isEnabled = false;
+        translate_button.isEnabled = false;*/
+        this.translator = TranslatorService(this, TranslateLanguage.FRENCH, TranslateLanguage.RUSSIAN, {
+            translate_button.isEnabled = true;
+            translate_button.isEnabled = true;
+        }) {
+            translate_button.isEnabled = false;
+            translate_button.isEnabled = false;
+            translated_text.text = "App can't download the translation model, please check your wifi connection..."
+        }
+
+        // translate button listener
+        translate_button.setOnClickListener {
+            this.translator.run(edit_query.text.toString()) { enText ->
+                translated_text.text = enText
+            }
+        }
+
+        // from language spinner listeners
+        to_language_spinner.setOnItemSelectedListener(this);
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        var lang_txt = parent.getItemAtPosition(pos).toString();
+        Toast.makeText(parent.context, lang_txt, Toast.LENGTH_LONG).show()
+        this.translator.to_language = "en"
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroy() {
+        this.translator.close()
+        super.onDestroy()
+    }
+}*/
+
+
+package fr.enssat.babelblock.jourdren_duchene
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.google.mlkit.nl.translate.TranslateLanguage
+import fr.enssat.babelblock.jourdren_duchene.services.translation.TranslatorService
+import fr.enssat.babelblock.jourdren_duchene.services.translation.languages
+import kotlinx.android.synthetic.main.activity_translator.*
+
+// inherit BaseActivity to manage menuInflater
+class TranslatorActivity : BaseActivity(), AdapterView.OnItemSelectedListener {
+
+    lateinit var translator: TranslatorService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // set activity's layout
+        setContentView(R.layout.activity_translator)
+
+
+        /* === SPINNERS INIT === */
+        // create to_language_spinner and from_language_spinner adapter
+        val ui_languages = languages.keys.toTypedArray()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ui_languages)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+
+
+        // populate from_language_spinner
+        val from_language_spinner = findViewById<Spinner>(R.id.from_language_spinner)
+        from_language_spinner.adapter = adapter
+        // set default langage
+        val default_from_language_spinner: Int = adapter.getPosition("french")
+        from_language_spinner.setSelection(default_from_language_spinner)
+
+
+
+        // populate to_language_spinner
+        val to_language_spinner = findViewById<Spinner>(R.id.to_language_spinner)
+        to_language_spinner.adapter = adapter
+        // set default langage
+        val default_to_language_spinner: Int = adapter.getPosition("english")
+        to_language_spinner.setSelection(default_to_language_spinner)
+
+
+
+        /* === SERVICES INIT === */
+        // create translation service
+        this.translator = TranslatorService(this, languages.get("french").toString(), TranslateLanguage.ENGLISH, {
             translate_button.isEnabled = true;
             translate_button.isEnabled = true;
         }, {
@@ -26,12 +136,47 @@ class TranslatorActivity : BaseActivity() {
             translated_text.text = "App can't download the translation model, please check your wifi connection..."
         })
 
-        // translate button listener
+
+        /* === BUTTON LISTENERS === */
         translate_button.setOnClickListener {
             this.translator.run(edit_query.text.toString()) { enText ->
                 translated_text.text = enText
             }
         }
+
+        // from language spinner listeners
+        to_language_spinner.setOnItemSelectedListener(this);
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        when(parent.id) {
+            R.id.from_language_spinner -> {
+                var to_lang_txt = parent.getItemAtPosition(pos).toString();
+                this.translator = TranslatorService(this, languages.get(to_lang_txt).toString(), languages.get("english").toString(), {
+                    translate_button.isEnabled = true;
+                    translate_button.isEnabled = true;
+                }, {
+                    translate_button.isEnabled = false;
+                    translate_button.isEnabled = false;
+                    translated_text.text = "App can't download the translation model, please check your wifi connection..."
+                })
+            }
+            R.id.to_language_spinner -> {
+                var from_lang_txt = parent.getItemAtPosition(pos).toString();
+                this.translator = TranslatorService(this, languages.get("french").toString(), languages.get(from_lang_txt).toString(), {
+                    translate_button.isEnabled = true;
+                    translate_button.isEnabled = true;
+                }, {
+                    translate_button.isEnabled = false;
+                    translate_button.isEnabled = false;
+                    translated_text.text = "App can't download the translation model, please check your wifi connection..."
+                })
+            }
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 
     override fun onDestroy() {
--- app/src/main/java/fr/enssat/babelblock/jourdren_duchene/services/translation/TranslatorService.kt
+++ app/src/main/java/fr/enssat/babelblock/jourdren_duchene/services/translation/TranslatorService.kt
@@ -1,17 +1,86 @@
+/*package fr.enssat.babelblock.jourdren_duchene.services.translation
+
+import android.content.Context
+import android.util.Log
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.Translator
+import com.google.mlkit.nl.translate.TranslatorOptions
+import fr.enssat.babelblock.jourdren_duchene.services.Service
+import java.util.Locale
+
+// we use MLKit way to say language (which are store in TranslateLanguage enum as strings)
+class TranslatorService(context: Context, from: String = TranslateLanguage.FRENCH, to: String = TranslateLanguage.ENGLISH, downloadModelSuccess: () -> Unit = {}, downloadModelFailed: () -> Unit = {}): Service(context) {
+
+    // MLKit translator instance
+    lateinit var translator: Translator
+
+    var from_language: String = from
+        set(new_from) { // we make the service able to modify local to change languages by using a setter
+            this.from_language = new_from
+            this.makeTranslator()
+        }
+    private var to_language: String = to
+        set(new_to) { // we make the service able to modify local to change languages by using a setter
+            this.to_language = new_to
+            this.makeTranslator()
+        }
+
+
+    // at init, make the model and download it
+    init {
+        this.makeTranslator(downloadModelSuccess, downloadModelFailed)
+    }
+
+
+    fun run(text: String, callback: (String) -> Unit) {
+        // execute translation
+        this.translator.translate(text).addOnSuccessListener(callback).addOnFailureListener { e -> Log.e("TranslatorService", "Translation failed", e) }
+    }
+
+    override fun close() {
+        this.translator.close()
+    }
+
+
+    // build translator instance and start downloading the needed model
+    private fun makeTranslator(downloadModelSuccess: () -> Unit = {}, downloadModelFailed: () -> Unit = {}) {
+        val translatorOptions = TranslatorOptions.Builder().setSourceLanguage(this.from_language).setTargetLanguage(this.to_language).build() // configure languages
+        this.translator = Translation.getClient(translatorOptions)
+        this.downloadModelIfNeeded(downloadModelSuccess, downloadModelFailed)
+    }
+
+    // download the model if needed
+    private fun downloadModelIfNeeded(downloadModelSuccess: () -> Unit = {}, downloadModelFailed: () -> Unit = {}) {
+        // apply the download condition => download if the model isn't on the device and that device is connected in wifi
+        this.translator.downloadModelIfNeeded(DownloadConditions.Builder().requireWifi().build())
+                .addOnSuccessListener {
+                    Log.d("TranslatorService", "Translation model download completed")
+                    downloadModelSuccess // callback success execution
+                }.addOnFailureListener {
+                    e -> Log.e("TranslatorService", "Translation model download failed ", e)
+                    downloadModelFailed // callback failure execution
+                }
+    }
+}*/
+
+
+
 package fr.enssat.babelblock.jourdren_duchene.services.translation
 
 import android.content.Context
 import android.util.Log
 import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
 import fr.enssat.babelblock.jourdren_duchene.services.Service
-import java.util.Locale
 
-class TranslatorService(context: Context, from: Locale, to: Locale, downloadModelSuccess: () -> Unit = {}, downloadModelFailed: () -> Unit = {}): Service(context) {
+class TranslatorService(context: Context, from: String, to: String, downloadModelSuccess: () -> Unit = {}, downloadModelFailed: () -> Unit = {}): Service(context) {
 
     private val downloadConditions = DownloadConditions.Builder().requireWifi().build() // make a condition object which describe condition needed to start downloading a model
-    private val translatorOptions = TranslatorOptions.Builder().setSourceLanguage(from.language).setTargetLanguage(to.language).build() // configure languages
+    private val translatorOptions = TranslatorOptions.Builder().setSourceLanguage(from).setTargetLanguage(to).build() // configure languages
     private val translator = Translation.getClient(translatorOptions)
 
     init {
