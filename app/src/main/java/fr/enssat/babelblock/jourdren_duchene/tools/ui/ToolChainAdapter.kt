5cac13c85128b853696e8885ecbc09113a268317
fjourdren <flavien.jourdren@gmail.com>
2020-11-26 15:41:49 +0100

    Change block design

--- app/src/main/java/fr/enssat/babelblock/jourdren_duchene/tools/ui/ToolChainAdapter.kt
+++ app/src/main/java/fr/enssat/babelblock/jourdren_duchene/tools/ui/ToolChainAdapter.kt
@@ -1,5 +1,6 @@
 package fr.enssat.babelblock.jourdren_duchene.tools.ui
 
+import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import fr.enssat.babelblock.jourdren_duchene.R
 import kotlinx.android.synthetic.main.list_item_tool_chain.view.*
 
-class ToolChainAdapter(val toolChain: ToolChain) : RecyclerView.Adapter<ToolChainAdapter.ToolViewHolder>(), ItemMoveAdapter {
+class ToolChainAdapter(val context: Context, val toolChain: ToolChain) : RecyclerView.Adapter<ToolChainAdapter.ToolViewHolder>(), ItemMoveAdapter {
 
     init {
         //notifyDataSetChanged() = redraw, the data set has changed
@@ -31,23 +32,51 @@ class ToolChainAdapter(val toolChain: ToolChain) : RecyclerView.Adapter<ToolChai
         notifyItemMoved(from, to)
     }
 
+    override fun onRowDeleted(target: Int) {
+        toolChain.remove(target)
+    }
+
     override fun onRowSelected(viewHolder: RecyclerView.ViewHolder) {
         viewHolder.itemView.setBackgroundColor(Color.GRAY)
     }
 
     override fun onRowReleased(viewHolder: RecyclerView.ViewHolder) {
-        viewHolder.itemView.setBackgroundColor(Color.TRANSPARENT)
+        viewHolder.itemView.setBackgroundColor(this.context.resources.getColor(R.color.block_color))
         notifyDataSetChanged()
     }
 
-    //viewholder, kind of reusable view cache,  for each tool in the chain
-    class ToolViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    // viewholder, kind of reusable view cache, for each tool in the chain
+    class ToolViewHolder(val view: View): RecyclerView.ViewHolder(view) {
         fun bind(toolChain: ToolChain, i: Int) {
+
+            // get the tool in rendering
             val tool = toolChain.get(i)
-            itemView.text_input.text = tool.input
-            itemView.text_output.text = tool.output
-            itemView.text_box.text = tool.title
-            itemView.text_box.setOnClickListener {
+
+
+            // rendering input and output fields management
+            if(tool.input == null) { // We show "nothing." if the string is null (if string is == "" => we don't show anything)
+                itemView.input_value.text = "Nothing."
+                itemView.input_value.setTextColor(this.view.context.resources.getColor(R.color.red))
+            } else {
+                itemView.input_value.text = tool.input
+                itemView.input_value.setTextColor(this.view.context.resources.getColor(R.color.default_in_out))
+            }
+
+            if(tool.output == null) { // We show "nothing." if the string is null (if string is == "" => we don't show anything)
+                itemView.output_value.text = "Nothing."
+                itemView.output_value.setTextColor(this.view.context.resources.getColor(R.color.red))
+            } else {
+                itemView.output_value.text = tool.output
+                itemView.output_value.setTextColor(this.view.context.resources.getColor(R.color.default_in_out))
+            }
+
+
+            // rendering tool title management
+            itemView.params.text = tool.title
+
+
+            // render tool in the pipeline
+            itemView.params.setOnClickListener {
                 toolChain.display(i)
             }
         }
