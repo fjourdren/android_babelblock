14360cf73cf7c7519ba5c0d2a7c966b910764a5a
fjourdren <flavien.jourdren@gmail.com>
2020-11-27 20:50:02 +0100

    Improve translation model management & add download model retry

--- app/src/main/java/fr/enssat/babelblock/jourdren_duchene/services/translation/TranslatorService.kt
+++ app/src/main/java/fr/enssat/babelblock/jourdren_duchene/services/translation/TranslatorService.kt
@@ -3,21 +3,28 @@ package fr.enssat.babelblock.jourdren_duchene.services.translation
 import android.content.Context
 import android.util.Log
 import com.google.mlkit.common.model.DownloadConditions
-import com.google.mlkit.nl.translate.TranslateLanguage
-import com.google.mlkit.nl.translate.Translation
-import com.google.mlkit.nl.translate.Translator
-import com.google.mlkit.nl.translate.TranslatorOptions
+import com.google.mlkit.common.model.RemoteModel
+import com.google.mlkit.common.model.RemoteModelManager
+import com.google.mlkit.nl.translate.*
 import fr.enssat.babelblock.jourdren_duchene.services.Service
+import java.util.*
 
 class TranslatorService: Service {
 
     var from_language: String = TranslateLanguage.FRENCH
         set(new_lang) {  // force build the translator with new language by using a setter
+            try {
+                this.deleteModel(this.from_language) // delete old from language model (adviced in the doc)
+            } catch (e: Exception) {}
+
             field = new_lang
             this.buildObject()
         }
     var to_language: String = TranslateLanguage.ENGLISH
         set(new_lang) {  // force build the translator with new language by using a setter
+            try {
+                this.deleteModel(this.to_language) // delete old to language model (adviced in the doc)
+            } catch (e: Exception) {}
             field = new_lang
             this.buildObject()
         }
@@ -25,10 +32,13 @@ class TranslatorService: Service {
     private lateinit var translatorOptions: TranslatorOptions
     private lateinit var translator: Translator
 
+    private val modelManager = RemoteModelManager.getInstance()
+
     constructor(context: Context, from_language: String, to_language: String): super(context) {
         this.from_language = from_language
         this.to_language = to_language
 
+        // we download model
         downloadModelIfNeeded()
     }
 
@@ -37,24 +47,58 @@ class TranslatorService: Service {
         this.translator = Translation.getClient(this.translatorOptions)
     }
 
-    fun downloadModelIfNeeded(downloadModelSuccess: () -> Unit = {}, downloadModelFailed: () -> Unit = {}) {
+    fun downloadModelIfNeeded(downloadModelSuccess: () -> Unit = {}, downloadModelFailed: () -> Unit = {}, finalDownloadModelFailed: () -> Unit = {}) {
         // make a condition object which describe condition needed to start downloading a model
         val downloadConditions = DownloadConditions.Builder().requireWifi().build()
 
         // apply the download condition => download if the model isn't on the device and that device is connected in wifi
         this.translator.downloadModelIfNeeded(downloadConditions)
                 .addOnSuccessListener {
-                    Log.d("TranslatorService", "Translation model download completed")
+                    Log.d("Model Management", "Translation model download completed")
                     downloadModelSuccess.invoke() // callback success execution
-                }.addOnFailureListener {
-                    e -> Log.e("TranslatorService", "Translation model download failed ", e)
+                }.addOnFailureListener { e -> Log.e("Model Management", "Translation model download failed. Retrying... ", e)
                     downloadModelFailed.invoke() // callback failure execution
+
+                    /***** Retry to download models but with lower level MLKit functions *****/
+                    // delete both models by hand if an error occurs
+                    this.deleteModel(this.from_language)
+                    this.deleteModel(this.to_language)
+
+                    // retry to re-download both models
+                    val modelFrom =  TranslateRemoteModel.Builder(this.from_language).build()
+                    val modelTo =  TranslateRemoteModel.Builder(this.to_language).build()
+
+                    // downloading two models
+                    modelManager.download(modelFrom, downloadConditions).addOnFailureListener {
+                        Log.e("Model Management", "Can't download $modelFrom model")
+                        finalDownloadModelFailed.invoke() // callback failure execution
+                    }
+                    modelManager.download(modelTo, downloadConditions).addOnFailureListener {
+                        Log.e("Model Management", "Can't download $modelTo model")
+                        finalDownloadModelFailed.invoke() // callback failure execution
+                    }
                 }
     }
 
     fun run(text: String, callback: (String) -> Unit) {
         // execute translation
         this.translator.translate(text).addOnSuccessListener(callback).addOnFailureListener { e -> Log.e("TranslatorService", "Translation failed", e) }
+    }
+
+    private fun deleteModel(languageToDelete: String) {
+        // get model to delete
+        val model =  TranslateRemoteModel.Builder(languageToDelete).build()
+        val modelRemote = model as RemoteModel
+
+        // if the model exists, to be sure that we try to delete an existing model (absolutely needed, otherwise our dynamic model deletion makes app crash)
+        this.modelManager.isModelDownloaded(modelRemote).addOnSuccessListener { modelExists ->
+            if(modelExists && model.language != Locale.getDefault().language && model.language != "en") { // check also that the model in deletion isn't the main phone language or english
+                Log.d("Model Management", "Deleting ${model.language} model")
+
+                // delete any previous downloaded models
+                this.modelManager.deleteDownloadedModel(model)
+            }
+        }.addOnFailureListener {}
     }
 
     override fun close() {
