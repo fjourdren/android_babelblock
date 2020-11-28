f3fb0455d4c258b6581ad1a84a22e43451c849b7
fjourdren <flavien.jourdren@gmail.com>
2020-11-28 18:16:10 +0100

    Add Text to speech locale selection

--- app/src/main/java/fr/enssat/babelblock/jourdren_duchene/TextToSpeechActivity.kt
+++ app/src/main/java/fr/enssat/babelblock/jourdren_duchene/TextToSpeechActivity.kt
@@ -1,17 +1,27 @@
 package fr.enssat.babelblock.jourdren_duchene
 
 import android.os.Bundle
-import fr.enssat.babelblock.jourdren_duchene.services.Service
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import fr.enssat.babelblock.jourdren_duchene.services.translation.languages
 import fr.enssat.babelblock.jourdren_duchene.services.tts.TextToSpeechService
-
 import kotlinx.android.synthetic.main.activity_text_to_speech.*
+import kotlinx.android.synthetic.main.activity_text_to_speech.edit_query
+import kotlinx.android.synthetic.main.activity_translator.*
 import java.util.*
 
 
 // inherit BaseActivity to manage menuInflater
-class TextToSpeechActivity : BaseActivity() {
+class TextToSpeechActivity : BaseActivity(), AdapterView.OnItemSelectedListener {
 
-    lateinit var service: TextToSpeechService;
+    lateinit var serviceTTS: TextToSpeechService
+
+    private var localesLanguagesUI = mutableListOf<String>()
+    private var localesLanguagesObjects = mutableListOf<Locale>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,18 +30,67 @@ class TextToSpeechActivity : BaseActivity() {
         setContentView(R.layout.activity_text_to_speech)
 
         // init text to speech service
-        this.service = TextToSpeechService(this.applicationContext, Locale.getDefault())
+        // use a callback to be sure that the speaker is initialized and that we can now use speaker's isLanguageAvailable func
+        this.serviceTTS = TextToSpeechService(this.applicationContext, Locale.getDefault()) {
+            /* === SPINNERS INIT === */
+            // create language_spinner
+            Locale.getAvailableLocales().forEach {
+                // this.serviceTTS.speaker.availableLanguages can be used for android > 5.0
+                if (it.country == "" && this.serviceTTS.speaker.isLanguageAvailable(it) >= 0) { // remove duplicated languages when this one is used in different countries (it just take the most basic one) && check that the speaker support this language
+                    localesLanguagesObjects.add(it) // add to localesLanguagesObjects list
+                }
+            }
 
-        // voice synthesizer button listener
-        play_button.setOnClickListener {
-            val text: String = edit_query.text.toString()
-            this.service.input = text // send text to the service
-            this.service.run()
+            // sort locales by displayLanguage
+            localesLanguagesObjects = localesLanguagesObjects.sortedBy { it.displayLanguage } as MutableList<Locale>
+
+            // foreach to make localesLanguagesUI with sorted localesLanguagesObjects (need to be done in two foreach because we can't order displayLanguage without doing that)
+            localesLanguagesObjects.forEach {
+                localesLanguagesUI.add(it.displayLanguage) // display language in the user's default locale
+            }
+
+            // make adapter
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, localesLanguagesUI)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            // populate from_language_spinner
+            language_spinner.adapter = adapter
+
+            // set default language_spinner value
+            language_spinner.setSelection(adapter.getPosition(Locale.getDefault().displayLanguage))
+
+
+            // voice synthesizer button listener
+            play_button.setOnClickListener {
+                val text: String = edit_query.text.toString()
+                this.serviceTTS.input = text // send text to the service
+                this.serviceTTS.run()
+            }
+        }
+
+
+        // language spinner listener
+        language_spinner.onItemSelectedListener = this;
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        when(parent.id) {
+            R.id.language_spinner -> {
+                // get language
+                val localeSelected: Locale = localesLanguagesObjects[pos]
+
+                // set from language
+                this.serviceTTS.locale = localeSelected
+            }
         }
     }
 
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+    }
+
     override fun onDestroy() {
-        this.service.close()
+        this.serviceTTS.close()
         super.onDestroy()
     }
 }
