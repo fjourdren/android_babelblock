b2e520173d9ed3f75a01c9907d4e6670f5050a2d
fjourdren <flavien.jourdren@gmail.com>
2020-12-06 22:41:53 +0100

    Start managing different holders

--- app/src/main/java/fr/enssat/babelblock/jourdren_duchene/services/pipeline/ToolChainAdapter.kt
+++ app/src/main/java/fr/enssat/babelblock/jourdren_duchene/services/pipeline/ToolChainAdapter.kt
@@ -8,16 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import fr.enssat.babelblock.jourdren_duchene.R
 import fr.enssat.babelblock.jourdren_duchene.services.translation.Language
 import fr.enssat.babelblock.jourdren_duchene.services.translation.TranslatorService
-import kotlinx.android.synthetic.main.activity_translator.*
+import fr.enssat.babelblock.jourdren_duchene.services.tts.TextToSpeechService
+import kotlinx.android.synthetic.main.activity_tts.view.*
 import kotlinx.android.synthetic.main.block_translate.view.*
 import java.util.*
 
 
-class ToolChainAdapter: RecyclerView.Adapter<ToolChainAdapter.ToolViewHolder>, ItemMoveAdapter {
+class ToolChainAdapter: Adapter<ViewHolder>, ItemMoveAdapter {
 
     override var itemsChain: ToolChain
     var context: Context
@@ -32,13 +34,41 @@ class ToolChainAdapter: RecyclerView.Adapter<ToolChainAdapter.ToolViewHolder>, I
 
     override fun getItemCount(): Int = this.itemsChain.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToolViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.block_translate, parent, false)
-        return ToolViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View
+        when(viewType) {
+            0 -> {
+                view = LayoutInflater.from(parent.context).inflate(R.layout.block_tts, parent, false)
+                return TTSHolder(view)
+            }
+            1 -> {
+                view = LayoutInflater.from(parent.context).inflate(R.layout.block_translate, parent, false)
+                return TranslateHolder(view)
+            }
+            else -> { // Note the block
+                Log.e("Error", "Not a valid ViewType")
+                throw Error("123")
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: ToolViewHolder, position: Int) {
-        holder.bind(this.itemsChain, position)
+    override fun getItemViewType(position: Int): Int {
+        Log.d("test", position.toString())
+        return position
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when (holder.getItemViewType()) {
+            0 -> {
+                (holder as TTSHolder).bind(this.itemsChain, position)
+            }
+            1 -> {
+                (holder as TranslateHolder).bind(this.itemsChain, position)
+            }
+            else -> { // Note the block
+                Log.e("Error", "Not a valid HolderType")
+            }
+        }
     }
 
     override fun onRowMoved(from: Int, to: Int) {
@@ -51,11 +81,11 @@ class ToolChainAdapter: RecyclerView.Adapter<ToolChainAdapter.ToolViewHolder>, I
         notifyDataSetChanged()
     }
 
-    override fun onRowSelected(viewHolder: RecyclerView.ViewHolder) {
+    override fun onRowSelected(viewHolder: ViewHolder) {
         viewHolder.itemView.setBackgroundColor(Color.GRAY)
     }
 
-    override fun onRowReleased(viewHolder: RecyclerView.ViewHolder) {
+    override fun onRowReleased(viewHolder: ViewHolder) {
         viewHolder.itemView.setBackgroundColor(this.context.resources.getColor(R.color.block_color))
     }
 
@@ -66,10 +96,94 @@ class ToolChainAdapter: RecyclerView.Adapter<ToolChainAdapter.ToolViewHolder>, I
 
 
 
+    // viewholder, kind of reusable view cache, for each tool in the chain
+    class TTSHolder(val view: View): ViewHolder(view) {
+        var localesLanguagesUI = mutableListOf<String>()
+        var localesLanguagesObjects = mutableListOf<Locale>()
+
+        lateinit var tool: Tool
+        lateinit var service: TextToSpeechService
+
+        fun bind(toolChain: ToolChain, i: Int) {
+
+            // get the tool in rendering
+            this.tool = toolChain.get(i)
+            this.service = tool.service as TextToSpeechService
+
+
+            // rendering tool title management
+            itemView.tool_title.text = tool.title
+
+
+
+
+            // rendering input and output fields management
+            if(tool.input == null || tool.input == "") { // We show "nothing." if the string is null or == ""
+                itemView.input_value.text = "Nothing."
+                itemView.input_value.setTextColor(this.view.context.resources.getColor(R.color.red))
+            } else {
+                itemView.input_value.text = tool.input
+                itemView.input_value.setTextColor(this.view.context.resources.getColor(R.color.default_in_out))
+            }
+
+            if(tool.output == null || tool.output == "") { // We show "nothing." if the string is null or == ""
+                itemView.output_value.text = "Nothing."
+                itemView.output_value.setTextColor(this.view.context.resources.getColor(R.color.red))
+            } else {
+                itemView.output_value.text = tool.output
+                itemView.output_value.setTextColor(this.view.context.resources.getColor(R.color.default_in_out))
+            }
+
+
+
+
+
+            // Spinner management
+            /* === SPINNERS INIT === */
+            // create adapter's array & sort locales by displayLanguage
+            localesLanguagesObjects = Language.generateMLKitAvailableLocales().sortedBy { it.displayLanguage } as MutableList<Locale>
+
+            // foreach to make localesLanguagesUI with sorted localesLanguagesObjects (need to be done in two foreach because we can't order displayLanguage without doing that)
+            localesLanguagesObjects.forEach {
+                localesLanguagesUI.add(it.displayLanguage) // display language in the user's default locale
+            }
+
+
+
+            // create to_language_spinner and from_language_spinner adapter
+            val adapter = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, localesLanguagesUI)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+
+
+
+
+            // populate from_language_spinner
+            view.language_spinner.adapter = adapter
+
+            // set default from_language_spinner
+            view.language_spinner.setSelection(adapter.getPosition(this.service.locale.displayLanguage))
+
+
+            // from spinner on item selected listener
+            view.language_spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    service.locale = localesLanguagesObjects[position]
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // another interface callback
+                }
+            }
+        }
+    }
+
+
+
 
 
     // viewholder, kind of reusable view cache, for each tool in the chain
-    class ToolViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+    class TranslateHolder(val view: View): ViewHolder(view) {
         var localesLanguagesUI = mutableListOf<String>()
         var localesLanguagesObjects = mutableListOf<Locale>()
 
