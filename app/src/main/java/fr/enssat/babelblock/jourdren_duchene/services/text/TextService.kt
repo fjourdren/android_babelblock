6dbeb31e678211537bee775ae80d5f05df87a214
jbduchenee <jean-baptiste.duchene@etudiant.univ-rennes1.fr>
2020-12-07 14:34:13 +0100

    Add text block

---
+++ app/src/main/java/fr/enssat/babelblock/jourdren_duchene/services/text/TextService.kt
@@ -0,0 +1,6 @@
+package fr.enssat.babelblock.jourdren_duchene.services.text
+
+import android.content.Context
+import fr.enssat.babelblock.jourdren_duchene.services.Service
+
+class TextService(context: Context) : Service(context) {}
