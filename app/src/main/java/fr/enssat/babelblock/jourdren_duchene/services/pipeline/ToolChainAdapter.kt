b99c96cd7d54bd5017d42e4ef9820447f9b78a77
fjourdren <flavien.jourdren@gmail.com>
2020-12-07 08:17:05 +0100

    Add STT & TTS support

--- app/src/main/java/fr/enssat/babelblock/jourdren_duchene/services/pipeline/ToolChainAdapter.kt
+++ app/src/main/java/fr/enssat/babelblock/jourdren_duchene/services/pipeline/ToolChainAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
@@ -11,17 +12,18 @@ import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import fr.enssat.babelblock.jourdren_duchene.R
+import fr.enssat.babelblock.jourdren_duchene.services.stt.Listener
 import fr.enssat.babelblock.jourdren_duchene.services.stt.SpeechToTextService
 import fr.enssat.babelblock.jourdren_duchene.services.translation.Language
 import fr.enssat.babelblock.jourdren_duchene.services.translation.TranslatorService
 import fr.enssat.babelblock.jourdren_duchene.services.tts.TextToSpeechService
-import kotlinx.android.synthetic.main.activity_tts.view.*
-import kotlinx.android.synthetic.main.activity_tts.view.language_spinner
+import kotlinx.android.synthetic.main.block_stt.view.*
 import kotlinx.android.synthetic.main.block_translate.view.*
 import kotlinx.android.synthetic.main.block_translate.view.input_value
 import kotlinx.android.synthetic.main.block_translate.view.output_value
 import kotlinx.android.synthetic.main.block_translate.view.tool_title
 import kotlinx.android.synthetic.main.block_tts.view.*
+import kotlinx.android.synthetic.main.block_tts.view.tool_tts_language_spinner
 import java.util.*
 
 
@@ -52,6 +54,10 @@ class ToolChainAdapter: Adapter<ViewHolder>, ItemMoveAdapter {
                 view = LayoutInflater.from(parent.context).inflate(R.layout.block_translate, parent, false)
                 return TranslateHolder(view)
             }
+            2 -> {
+                view = LayoutInflater.from(parent.context).inflate(R.layout.block_stt, parent, false)
+                return STTHolder(view)
+            }
             else -> { // Note the block
                 Log.e("Error", "Not a valid ViewType")
                 throw Error("Not a valid ViewType")
@@ -64,6 +70,8 @@ class ToolChainAdapter: Adapter<ViewHolder>, ItemMoveAdapter {
             return 0
         } else if(this.itemsChain[position].service is TranslatorService) {
             return 1
+        } else if(this.itemsChain[position].service is SpeechToTextService) {
+            return 2
         }
 
         return -1
@@ -76,6 +84,9 @@ class ToolChainAdapter: Adapter<ViewHolder>, ItemMoveAdapter {
             }
             1 -> {
                 (holder as TranslateHolder).bind(this.itemsChain, position)
+            }
+            2 -> {
+                (holder as STTHolder).bind(this.itemsChain, position)
             }
             else -> { // Note the block
                 Log.e("Error", "Not a valid HolderType")
@@ -110,9 +121,6 @@ class ToolChainAdapter: Adapter<ViewHolder>, ItemMoveAdapter {
 
     // viewholder, kind of reusable view cache, for each tool in the chain
     class TTSHolder(val view: View): ViewHolder(view) {
-        var localesLanguagesUI = mutableListOf<String>()
-        var localesLanguagesObjects = mutableListOf<Locale>()
-
         lateinit var tool: Tool
         lateinit var service: TextToSpeechService
 
@@ -150,31 +158,37 @@ class ToolChainAdapter: Adapter<ViewHolder>, ItemMoveAdapter {
 
 
 
+
             // Spinner management
             /* === SPINNERS INIT === */
-            // create adapter's array & sort locales by displayLanguage
-            localesLanguagesObjects = Language.generateMLKitAvailableLocales().sortedBy { it.displayLanguage } as MutableList<Locale>
+            var localesLanguagesUI = mutableListOf<String>()
+            var localesLanguagesObjects = mutableListOf<Locale>()
+
+            // create language_spinner
+            Locale.getAvailableLocales().forEach {
+                // this.serviceTTS.speaker.availableLanguages can be used for android > 5.0
+                if (it.country == "") { // remove duplicated languages when this one is used in different countries (it just take the most basic one) && check that the speaker support this language
+                    localesLanguagesObjects.add(it) // add to localesLanguagesObjects list
+                }
+            }
+
+            // sort locales by displayLanguage
+            localesLanguagesObjects = localesLanguagesObjects.sortedBy { it.displayLanguage } as MutableList<Locale>
 
             // foreach to make localesLanguagesUI with sorted localesLanguagesObjects (need to be done in two foreach because we can't order displayLanguage without doing that)
             localesLanguagesObjects.forEach {
                 localesLanguagesUI.add(it.displayLanguage) // display language in the user's default locale
             }
 
-
-
-            // create to_language_spinner and from_language_spinner adapter
-            val adapter = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, localesLanguagesUI)
+            // make adapter
+            val adapter = ArrayAdapter(this.view.context, android.R.layout.simple_spinner_item, localesLanguagesUI)
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-
-
-
-
-            // populate from_language_spinner
+            // populate language_spinner
             view.tool_tts_language_spinner.adapter = adapter
 
-            // set default from_language_spinner
-            view.tool_tts_language_spinner.setSelection(adapter.getPosition(this.service.locale.displayLanguage))
+            // set default language_spinner value
+            view.tool_tts_language_spinner.setSelection(adapter.getPosition(Locale.getDefault().displayLanguage))
 
 
             // from spinner on item selected listener
@@ -186,6 +200,13 @@ class ToolChainAdapter: Adapter<ViewHolder>, ItemMoveAdapter {
                 override fun onNothingSelected(parent: AdapterView<*>?) {
                     // another interface callback
                 }
+            }
+
+
+            // tool_tts_play_button listener
+            view.tool_tts_play_button.setOnClickListener {
+                this.service.input = tool.input
+                this.service.run()
             }
         }
     }
@@ -201,8 +222,6 @@ class ToolChainAdapter: Adapter<ViewHolder>, ItemMoveAdapter {
 
         lateinit var tool: Tool
         lateinit var serviceTranslator: TranslatorService
-
-        var nbSpinnerNotify = 0 // fix to not call spinner's listener when there is a notifyDataSetChanged()
 
         fun bind(toolChain: ToolChain, i: Int) {
 
@@ -327,6 +346,123 @@ class ToolChainAdapter: Adapter<ViewHolder>, ItemMoveAdapter {
             }, {
                 itemView.tool_translate_info_message.text = "Error: App can't download the translation model, please check your wifi connection or used languages..."
             })
+        }
+    }
+
+
+
+    // viewholder, kind of reusable view cache, for each tool in the chain
+    class STTHolder(val view: View): ViewHolder(view) {
+        var localesLanguagesUI = mutableListOf<String>()
+        var localesLanguagesObjects = mutableListOf<Locale>()
+
+        lateinit var tool: Tool
+        var service: SpeechToTextService = SpeechToTextService(view.context, Locale.getDefault(), object: Listener {
+            override fun onResult(text: String, final: Boolean) {
+                Log.d("BlockSpeechToText", "Final: $text")
+                tool.output = text
+                itemView.output_value.text = tool.output
+                itemView.output_value.setTextColor(view.context.resources.getColor(R.color.default_in_out))
+            }
+        })
+
+        fun bind(toolChain: ToolChain, i: Int) {
+
+            // get the tool in rendering
+            this.tool = toolChain.get(i)
+
+
+            // rendering tool title management
+            itemView.tool_title.text = tool.title
+
+
+            itemView.input_value.text = tool.input
+            itemView.output_value.text = tool.output
+
+
+
+
+            // rendering input and output fields management
+            if(tool.input == null || tool.input == "") { // We show "nothing." if the string is null or == ""
+                itemView.input_value.text = "Nothing."
+                itemView.input_value.setTextColor(this.view.context.resources.getColor(R.color.red))
+            } else {
+                itemView.input_value.text = tool.input
+                itemView.input_value.setTextColor(this.view.context.resources.getColor(R.color.default_in_out))
+            }
+
+            if(tool.output == null || tool.output == "") { // We show "nothing." if the string is null or == ""
+                itemView.output_value.text = "Nothing."
+                itemView.output_value.setTextColor(this.view.context.resources.getColor(R.color.red))
+            } else {
+                itemView.output_value.text = tool.output
+                itemView.output_value.setTextColor(this.view.context.resources.getColor(R.color.default_in_out))
+            }
+
+
+
+
+
+            // Spinner management
+            /* === SPINNERS INIT === */
+            // create adapter's array & sort locales by displayLanguage
+            localesLanguagesObjects = Language.generateMLKitAvailableLocales().sortedBy { it.displayLanguage } as MutableList<Locale>
+
+            // foreach to make localesLanguagesUI with sorted localesLanguagesObjects (need to be done in two foreach because we can't order displayLanguage without doing that)
+            localesLanguagesObjects.forEach {
+                localesLanguagesUI.add(it.displayLanguage) // display language in the user's default locale
+            }
+
+
+
+            // create to_language_spinner and from_language_spinner adapter
+            val adapter = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, localesLanguagesUI)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+
+
+
+
+            // populate from_language_spinner
+            view.tool_stt_language_spinner.adapter = adapter
+
+            // set default from_language_spinner
+            view.tool_stt_language_spinner.setSelection(adapter.getPosition(this.service.locale.displayLanguage))
+
+
+            // from spinner on item selected listener
+            view.tool_stt_language_spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    service.locale = localesLanguagesObjects[position]
+                    tool.output = tool.input
+
+                    if(tool.output == null || tool.output == "") { // We show "nothing." if the string is null or == ""
+                        itemView.output_value.text = "Nothing."
+                        itemView.output_value.setTextColor(view.context.resources.getColor(R.color.red))
+                    } else {
+                        itemView.output_value.text = tool.output
+                        itemView.output_value.setTextColor(view.context.resources.getColor(R.color.default_in_out))
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // another interface callback
+                }
+            }
+
+
+            // record_button listener
+            view.tool_stt_record_button.setOnTouchListener { v, event ->
+                if(event.action == MotionEvent.ACTION_DOWN) {
+                    Log.d("SpeechToTextActivity UI", "STT button pressed")
+                    v.performClick()
+                    this.service.run()
+                } else if(event.action == MotionEvent.ACTION_UP) {
+                    Log.d("SpeechToTextActivity UI", "STT button released")
+                    this.service.stop()
+                }
+                false
+            }
         }
     }
 }
